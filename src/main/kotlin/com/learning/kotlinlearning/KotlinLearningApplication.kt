9cdbe7659db38494825f90a2d84ee9040dc0f072
pancudaniel7 <pancudaniel7@gmail.com>
2022-11-20 20:23:07 +0200

    Rest controller - Added controller - Added gitingore

--- src/main/kotlin/com/learning/kotlinlearning/KotlinLearningApplication.kt
+++ src/main/kotlin/com/learning/kotlinlearning/KotlinLearningApplication.kt
@@ -2,10 +2,21 @@ package com.learning.kotlinlearning
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KotlinLearningApplication
 
 fun main(args: Array<String>) {
 	runApplication<KotlinLearningApplication>(*args)
+}
+
+@RestController
+class TestRemoteDebugController {
+
+	@GetMapping("/test")
+	fun ping(): String {
+		return "ping!"
+	}
 }
